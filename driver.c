5981785fc00925524a4206ef3bedb1e612767ae5
Yuhan Lin <linyuhan0315@hotmail.com>
2017-11-26 13:57:20 -0500

    Fixed driver

--- driver.c
+++ driver.c
@@ -12,18 +12,27 @@
 #include <string.h>
 #include <stdlib.h>
 
+static void changeExtension(char_t* dest, const char_t* src, const char_t* ext){
+    strcpy(dest, src);
+    char_t* pDot = strchr(dest, '.');
+    if (pDot == NULL){
+        pDot = dest + strlen(dest);
+    }
+    else{
+        pDot++;
+    }
+    sprintf(pDot, ext);
+}
+
 int driver(int argc, char_t const *argv[])
 {
-    if (argc < 3){
-        fprintf(stderr, "Error: Need an input and an output file.\n");
+    if (argc < 2){
+        fprintf(stderr, "Error: Need an input file.\n");
         return 2;
     }
     const char_t* infilename = argv[1];
-    const char_t* outfilename = argv[2];
-    if (!strcmp(infilename, outfilename)){
-        fprintf(stderr, "Error: Input and output files can't be the same.\n");
-        return 2;
-    }
+    char_t outfilename[100];
+    changeExtension(outfilename, infilename, "s");
     openFiles(infilename, outfilename);
 
     initLexer();
@@ -48,8 +57,10 @@ int driver(int argc, char_t const *argv[])
     closeFiles(infilename, outfilename);
 
     if (code == 0){
-        char_t gccCommand[1000];
-        sprintf(gccCommand, "gcc %s", outfilename);
+        char_t exefilename[100];
+        changeExtension(exefilename, infilename, "exe");
+        char_t gccCommand[100];
+        sprintf(gccCommand, "gcc %s -o %s", outfilename, exefilename);
         return system(gccCommand);
     }
     return code;
--- test/maintest.c
+++ test/maintest.c
@@ -1,21 +1,19 @@
 #include "utils.h"
 #include "test/utils.c"
 #include <stdlib.h>
-
 int driver(int argc, char_t const *argv[]);
 
 const char_t* CFILES[] = {"basic.c", "binop.c", "params.c", "unop.c", "void.c"};
-const char_t* SFILES[] = {"basic.s", "binop.s", "params.s", "unop.s", "void.s"};
+const char_t* EXEFILES[] = {"basic.exe", "binop.exe", "params.exe", "unop.exe", "void.exe"};
 const int EXPECTED_OUT[] = {1, 46, 1, 3, 1};
 const int FILE_COUNT = 5;
 
 static void testDriver(int i){
     DITCH_LEVEL = 1;
-    const char_t *driverArgs[3];
+    const char_t *driverArgs[2];
     driverArgs[1] = CFILES[i];
-    driverArgs[2] = SFILES[i];
-    assertEqNum(driver(3, driverArgs), 0);
-    assertEqNum(system("a.exe"), EXPECTED_OUT[i]);
+    assertEqNum(driver(2, driverArgs), 0);
+    assertEqNum(system(EXEFILES[i]), EXPECTED_OUT[i]);
 }
 
 int main(int argc, char const *argv[])
@@ -24,4 +22,4 @@ int main(int argc, char const *argv[])
         testDriver(i);
     }
     return 0;
-} 
+}
